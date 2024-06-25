efb177aa4eab559613e643c3f950c3c7d9bc2073
Emilo067 <gasanovemil467@gmail.com>
2024-06-25 23:05:40 +0300

    create AppRouter.tsx

---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,17 @@
+import React, {Suspense} from 'react';
+import {Route, Routes} from "react-router-dom";
+import {MainPage} from "pages/MainPage";
+import {AboutPage} from "pages/AboutPage";
+import {routeConfig} from "shared/config/routeConfig/routeConfig";
+
+export const AppRouter = () => {
+    return (
+        <Suspense fallback={<h1>Loading</h1>}>
+            <Routes>
+                {Object.values(routeConfig).map(({element, path}) => {
+                    return <Route key={path} path={path} element={element}/>
+                })}
+            </Routes>
+        </Suspense>
+    );
+};

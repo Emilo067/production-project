c9303f19782372e7d9f7beab982cd0db7563ac00
Emilo067 <gasanovemil467@gmail.com>
2024-07-15 18:07:01 +0300

    add test for Login module

---
+++ src/features/AuthByUsername/model/selectors/getLoginPassword/getLoginIsPassword.test.ts
@@ -0,0 +1,19 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getLoginPassword } from './getLoginPassword';
+
+describe('getLoginError.test', () => {
+    test('should return value', () => {
+        const state: DeepPartial<StateSchema> = {
+            loginForm: {
+                password: '1234',
+            },
+        };
+        expect(getLoginPassword(state as StateSchema)).toEqual('1234');
+    });
+
+    test('should return empty string', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getLoginPassword(state as StateSchema)).toEqual('');
+    });
+});
--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -11,6 +11,7 @@ interface LoginByUsernameProps {
 export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { rejectValue: string }>(
     'login/loginByUsername',
     async (authData, thunkAPI) => {
+        const { dispatch, rejectWithValue } = thunkAPI;
         try {
             const response = await axios.post<User>('http://localhost:8000/login', authData);
 
@@ -19,12 +20,12 @@ export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { re
             }
 
             localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
-            thunkAPI.dispatch(userActions.setAuthData(response.data));
+            dispatch(userActions.setAuthData(response.data));
 
             return response.data;
         } catch (e) {
             console.log(e);
-            return thunkAPI.rejectWithValue('error');
+            return rejectWithValue('error');
         }
     },
 );

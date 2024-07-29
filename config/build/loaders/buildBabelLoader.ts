d45010292669c9d194705b9f3f089df5537e99cc
Emilo067 <gasanovemil467@gmail.com>
2024-07-29 13:08:41 +0300

    update app router and any fixed

---
+++ config/build/loaders/buildBabelLoader.ts
@@ -0,0 +1,25 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from '../types/config';
+
+export function buildBabelLoader(isDev: boolean) {
+    return {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        'i18next-extract',
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true,
+                        },
+                    ],
+                    isDev && require.resolve('react-refresh/babel'),
+                ].filter(Boolean),
+            },
+        },
+    };
+}
